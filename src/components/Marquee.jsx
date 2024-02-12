919e19b3e31493cb1427fd5e036d8946b865c80b
Sameer <ccontact.sameer04@gmail.com>
2024-02-13 00:23:24 +0530

    The OCHI clone

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -1,33 +1,18 @@
-import { motion } from "framer-motion";
+import { motion } from 'framer-motion'
+import React from 'react'
 
-const Marquee = () => {
+function Marquee() {
   return (
-    <div
-      data-scroll
-      data-scroll-section
-      data-scroll-speed=".2"
-      className="w-full py-10 rounded-tl-3xl rounded-tr-3xl bg-[#004D43]"
-    >
-      <div className="text border-t-2 border-b-2 border-zinc-300 overflow-hidden flex whitespace-nowrap gap-[10vw] ">
-        <motion.h1
-          initial={{ x: 0 }}
-          animate={{ x: "-100%" }}
-          transition={{ ease: "linear", repeat: Infinity, duration: 10 }}
-          className="text-[18vw] leading-none font-['century_gothic'] font-bold uppercase tracking-tightest pr-20"
-        >
-          we are ochi
-        </motion.h1>
-        <motion.h1
-          initial={{ x: 0 }}
-          animate={{ x: "-100%" }}
-          transition={{ ease: "linear", repeat: Infinity, duration: 10 }}
-          className="text-[18vw] leading-none font-['century_gothic'] font-bold uppercase tracking-tightest pr-20"
-        >
-          we are ochi
-        </motion.h1>
+    <div  data-scroll  data-scroll-section  data-scroll-speed=".1"   className='w-full py-10 rounded-tr-3xl  rounded-tl-3xl  bg-[#004D43]' >
+      <div className='text flex  overflow-hidden  whitespace-nowrap   border-b-2  border-t-2  ' >
+<motion.h1 initial={{x:0}} animate={{x:"-100%"}} transition={{ repeat : Infinity ,  ease:"linear" , duration : 5   }}    className='text-[13vw] leading-none -mb-10  pr-20  font-bold  font-["Founders Grotesk Condensed"]' >WE ARE OCHI   </motion.h1>
+<motion.h1 initial={{x:0}} animate={{x:"-100%"}} transition={{ repeat : Infinity ,  ease:"linear" , duration :5 }}  className='text-[13vw] leading-none   font-bold pr-20 font-["Founders Grotesk Condensed"] ' >WE ARE OCHI</motion.h1>
+<motion.h1 initial={{x:0}} animate={{x:"-100%"}} transition={{ repeat : Infinity ,  ease:"linear" , duration :5 }}  className='text-[13vw] leading-none   font-bold pr-20 font-["Founders Grotesk Condensed"] ' >WE ARE OCHI</motion.h1>
+
+
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Marquee;
+export default Marquee
